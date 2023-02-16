874042300cf6bcdfef58cfac77f8b11c8329e41f
Alex Cohen <aecohen@mit.edu>
2023-02-15 23:05:00 -0500

    populating all places in kendall square

--- Free2Pee/src/Free2Pee.jl
+++ Free2Pee/src/Free2Pee.jl
@@ -1,5 +1,9 @@
 module Free2Pee
-
-
+using JSON3, Downloads
+get_site(url) = take!(Downloads.download(url, IOBuffer()))
+get_json(url) = JSON3.read(get_site(url))
+const DATADIR = joinpath(@__DIR__, "../data/kendall_places_json/")
+data(x) = joinpath(DATADIR, x)
+export get_site, get_json
 
 end # module Free2Pee
--- Free2Pee/test/populate_bathrooms.jl
+++ Free2Pee/test/populate_bathrooms.jl
@@ -2,23 +2,38 @@ cd(@__DIR__)
 using Pkg
 Pkg.activate("..")
 Pkg.instantiate()
-
+using Free2Pee
 using JSON3, Downloads, CSV, DataFrames, JSONTables
 using HTTP, URIs
 
-key = read("key.txt", String);
+key = read(joinpath(@__DIR__, "../../key.txt"), String);
 
 # somewhere close to MIT
 lat = 42.362125
 long = -71.098694
-url5 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=$(lat)%2C$(long)&radius=500&type=restaurant&key=$(key)";
-j5 = get_json(url5)
-r = j5.results
-df = DataFrame(jsontable(r))
-chipotle = r[findfirst(x->x.name == "Chipotle Mexican Grill", r)]
-@test chipotle.name == "Chipotle Mexican Grill"
-place_id = chipotle.place_id
-CSV.write("nearbysearch.csv", df)
-# Warning: If you do not specify at least one field with a request, or if you omit the fields parameter from a request, ALL possible fields will be returned, and you will be billed accordingly. This applies only to Place Details requests.
-url6 = "https://maps.googleapis.com/maps/api/place/details/json?place_id=$(place_id)&key=$key";
-j6 = get_json(url6)
+url_kendall = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=$(lat)%2C$(long)&radius=1600&type=restaurant&key=$(key)";
+j_kendall = get_json(url_kendall)
+all_res = []
+kendall_results = j_kendall.results
+append!(all_res, kendall_results)
+
+while haskey(j_kendall,:next_page_token) && j_kendall.next_page_token != nothing
+    println("here")
+    url_kendall  = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=$(j_kendall.next_page_token)&keyword=food&location=$(lat)%2C$(long)&radius=1600&type=restaurant&key=$(key)";
+    println(url_kendall)
+    j_kendall = get_json(url_kendall)
+    println(j_kendall)
+    println(j_kendall.next_page_token)
+    kendall_results = j_kendall.results
+    append!(all_res, kendall_results)
+end
+
+kendall_results = j_kendall.results
+kendall_df = DataFrame(jsontable(kendall_results))
+for pid in kendall_df.place_id
+    url_pid = "https://maps.googleapis.com/maps/api/place/details/json?place_id=$(pid)&key=$key";
+    j_pid = get_json(url_pid)
+    write(Free2Pee.data(replace(j_pid.result.name*".json", " "=>"_")), JSON3.write(j_pid))
+end
+
+# name, hours, business_status, address, place id, prob of bathroom, uncertainty, bathroom id
