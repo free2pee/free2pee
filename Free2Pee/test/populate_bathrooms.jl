5055d91f1ca99606f17a23779fbddbbd99c191eb
Alex Cohen <aecohen@mit.edu>
2023-02-15 23:37:13 -0500

    Co-authored-by: anand jain <anandijain@users.noreply.github.com> Co-authored-by: Michael Fortunato  <Michael.N.Fortunato@gmail.com>

--- Free2Pee/test/populate_bathrooms.jl
+++ Free2Pee/test/populate_bathrooms.jl
@@ -4,36 +4,43 @@ Pkg.activate("..")
 Pkg.instantiate()
 using Free2Pee
 using JSON3, Downloads, CSV, DataFrames, JSONTables
-using HTTP, URIs
+using HTTP, URIs, Test
 
 key = read(joinpath(@__DIR__, "../../key.txt"), String);
 
 # somewhere close to MIT
-lat = 42.362125
-long = -71.098694
-url_kendall = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=$(lat)%2C$(long)&radius=1600&type=restaurant&key=$(key)";
-j_kendall = get_json(url_kendall)
-all_res = []
-kendall_results = j_kendall.results
-append!(all_res, kendall_results)
+lat_lon = [
+    "kendall" => (42.36261269868649, -71.08557312482398),
+ "central" => (42.36245846895235, -71.10484457066728),
+"harvard" => (42.37343282907278, -71.11880134391764),
+"porter" => (42.388526745545576, -71.11911680928606),
+"davis" => (42.39684669768773, -71.12170552286885),
+"alewife" => (42.39590419344493, -71.14152717257815)
+]
+locations = Dict(lat_lon)
 
-while haskey(j_kendall,:next_page_token) && j_kendall.next_page_token != nothing
-    println("here")
-    url_kendall  = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=$(j_kendall.next_page_token)&keyword=food&location=$(lat)%2C$(long)&radius=1600&type=restaurant&key=$(key)";
-    println(url_kendall)
-    j_kendall = get_json(url_kendall)
-    println(j_kendall)
-    println(j_kendall.next_page_token)
-    kendall_results = j_kendall.results
-    append!(all_res, kendall_results)
+all_res = []
+for (stop, (lat, long)) in locations
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=food&location=$(lat)%2C$(long)&radius=1600&type=restaurant&key=$(key)";
+    j = get_json(url)
+    results = j.results
+    append!(all_res, results)
+
+    while haskey(j,:next_page_token) && j.next_page_token != nothing
+        sleep(3)
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=$(j.next_page_token)&keyword=food&location=$(lat)%2C$(long)&radius=1600&type=restaurant&key=$(key)";
+        j = get_json(url)
+        results = j.results
+        append!(all_res, results)
+    end
 end
 
-kendall_results = j_kendall.results
-kendall_df = DataFrame(jsontable(kendall_results))
-for pid in kendall_df.place_id
+all_res = map(identity, all_res)
+df = DataFrame(jsontable(JSON3.read(JSON3.write(all_res))))
+@test nrow(df) == 360
+
+for pid in df.place_id
     url_pid = "https://maps.googleapis.com/maps/api/place/details/json?place_id=$(pid)&key=$key";
     j_pid = get_json(url_pid)
     write(Free2Pee.data(replace(j_pid.result.name*".json", " "=>"_")), JSON3.write(j_pid))
 end
-
-# name, hours, business_status, address, place id, prob of bathroom, uncertainty, bathroom id
