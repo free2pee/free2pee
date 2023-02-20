eba9b48436d58f46db6d3753ba608f69c1f3fef2
anand jain <anandj@uchicago.edu>
2023-02-20 17:36:53 -0500

    make a useful toilet query in overpass QL, add the taginfo project for open toilet map, and polish the rest api in oxygen
    Co-authored-by: Alexander E Cohen <acoh64@users.noreply.github.com>

--- Free2Pee/test/overpass_api.jl
+++ Free2Pee/test/overpass_api.jl
@@ -3,17 +3,18 @@ using JSON3, Downloads, CSV, DataFrames, JSONTables, PrettyTables
 using HTTP, URIs
 using Distances
 
-lat = 42.3640906
-lon = -71.1018806
-()
+# lat = 42.3640906
+# lon = -71.1018806
+const LAT = 42.35810736568732
+const LON = -71.10592447014132
+lat = LAT
+lon = LON
 url = "https://overpass-api.de/api/interpreter"
-maps_uri = URI("https://www.google.com/maps/dir/")
-h = HTTP.get(test_uri)
-
+const MAPS_URI = URI("https://www.google.com/maps/dir/")
+# h = HTTP.get(test_uri)
+# 4360543295
 example_query = "https://www.google.com/maps/dir/?api=1&origin=<lat, long>&destination=<lat, long>"
-u = URI(example_query)
-HTTP.queryparams(u)
-dump(ex)
+
 get_site(url) = take!(Downloads.download(url, IOBuffer()))
 get_json(url) = JSON3.read(get_site(url))
 
@@ -21,40 +22,66 @@ function lat_long_pair(n)
     return (n["lat"], n["lon"])
 end
 
-function generate_google_api_url(origin_lat,origin_long, dest_lat, dest_long) 
-    URI(maps_uri; query=Dict("api" => 1, "origin" => "$origin_lat,$origin_long", "destination" => "$dest_lat,$dest_long", "dir_action" => "navigate")) 
-end 
-
-
-@get "/{lat}/{lon}" function (req::HTTP.Request, lat, lon)
-    lat = parse(Float64, lat)
-    lon = parse(Float64, lon)
-    # todo make radius query parameter
-    query = """
-        [out:json];
-        node(around:5000, $lat, $lon)["amenity"="toilets"];
-        out 10;
-    """
-
+function to_df(lat, lon)
+    query = gen_query(lat, lon)
     response = HTTP.post(url, nothing, query)
     j = JSON3.read(response.body)
     es = j.elements
-    # e = es[1]
-    # elat, elon = e.lat, e.lon
+    isempty(es) && error("no results")
 
     ps = lat_long_pair.(es)
     distances = haversine.(ps, ((lat, lon),))
-    sorted_ns = es[sortperm(distances)]
-    df = DataFrame(sorted_ns)
-
+    # sorted_ns = es[sortperm(distances)]
+    df = DataFrame(es)
+    df.distance = sort(distances)
     urls = map(x -> generate_google_api_url(lat, lon, x...), ps)
     df.maps_url = urls
-    view_df = df[:, [:id, :lat, :lon, :maps_url]]
-
-    io = IOBuffer()
-    pretty_table(io, view_df, nosubheader=true, backend=Val(:html))
-    HTTP.Response(200, ["Content-Type" => "text/html"]; body=take!(io))
-
+    df 
 end
 
+function node_string(tag)
+    if haskey(tag, :value)
+        """[\"$(tag.key)\"=\"$(tag.value)\"];"""
+    else
+        """[\"$(tag.key)\"];"""
+    end
+end
+
+function generate_google_api_url(origin_lat,origin_long, dest_lat, dest_long) 
+    URI(MAPS_URI; query=Dict("api" => 1, "origin" => "$origin_lat,$origin_long", "destination" => "$dest_lat,$dest_long", "dir_action" => "navigate", "travelmode" => "walking")) 
+end
+
+@get "/{lat}/{lon}" function foo(req::HTTP.Request, lat, lon)
+    lat = parse(Float64, lat)
+    lon = parse(Float64, lon)
+    df = to_df(lat, lon)
+    view_df = df[:, [:id, :distance, :lat, :lon, :maps_url]]
+    sort!(view_df, :distance)
+    io = IOBuffer()
+    pretty_table(io, view_df, nosubheader=true, backend=Val(:html))
+    
+    HTTP.Response(200, ["Content-Type" => "text/html"]; body=take!(io))
+end
+
+@get "/" function (req::HTTP.Request)
+    HTTP.Response(200, ["Content-Type" => "text/html"]; body="""
+    <html>
+    <head>
+    <title>Free2Pee</title>
+    </head>
+    <body>
+    <h1>Free2Pee</h1>
+    <p>Find the nearest public toilets</p>
+    <form action="/$LAT/$LON">
+    <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
+    """)
+end
+
+# serve(;host="10.0.0.224")
 serve()
+
+# try this not haversine
+# curl 'http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false'
